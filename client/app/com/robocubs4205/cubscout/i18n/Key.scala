b46d7dc71c6205dfceb265f0cab08fc43c982eed
trevor <tgiddings000@gmail.com>
2017-09-14 21:42:02 -0700

    rename key nesting operator to ~>
    since -> is taken by pair-making for maps

--- client/app/com/robocubs4205/cubscout/i18n/Key.scala
+++ client/app/com/robocubs4205/cubscout/i18n/Key.scala
@@ -3,7 +3,7 @@ package com.robocubs4205.cubscout.i18n
 import scala.language.implicitConversions
 
 case class Key(value:String) {
-  def ->(key:Key) = Key(s"$value.${key.value}")
+  def ~>(key:Key) = Key(s"$value.${key.value}")
   def parent: Option[Key] = {
     val parentString = value.split("\\.").dropRight(1).mkString(".")
     if (parentString.isEmpty) None else Some(Key(parentString))
